bfe36655c4e5462fae22f6b34b6a298d0b0d3d78
unknown <sharma78655533@gmail.com>
2023-02-22 22:48:49 +0530

    location page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import AllRoutes from "./Pages/Home/AllRoutes";
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <AllRoutes/>
+    </div>
   )
 }
 
--- frontend/src/Pages/Home/Location.jsx
+++ frontend/src/Pages/Home/Location.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, Heading, Divider, Input  } from '@chakra-ui/react';
+import {GrLocation, GrMapLocation} from "react-icons/gr"
+import {TbCurrentLocation} from "react-icons/tb"
 
 const Location = () => {
+
+const [location, setLocation] = useState(data1[0]);
+
+const handleLocation = (el)=>{
+   setLocation(el)
+}
+
+
   return (
     <div>
-        <h1 style={{fontSize:'30px', textAlign:'center'}} >Location </h1>
+        <Box style={{width:'65%', margin:'auto', marginTop:"80px",padding:"30px 0px",boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}} >
+          <Box style={{display:"flex",justifyContent:'space-evenly',alignItems:'center'}} >
+            <Box width="40%" ><Input type="text" placeholder="Enter location" value={location}  /></Box>
+            <Box style={{display:"flex", alignItems:"center"}} ><TbCurrentLocation fontSize='30px' /> Current Location</Box>
+            <Box style={{display:"flex", alignItems:"center"}}><GrMapLocation fontSize='30px' />Select Location on Map</Box>
+          </Box>
+
+          <Box style={{display:"flex",justifyContent:'space-around',marginTop:"30px" , backgroundColor:'#f5f5f5',padding:"30px 0px" }}>
+
+            <Box  >
+              <Box>RECENTLY SEARCHED LOCATIONS</Box>
+              {data1.map((el)=>(
+                <Box style={{height:"70px",
+                display:"flex",borderRadius:"8px",
+                 alignItems:"center",marginBottom:"6px",backgroundColor:'white',padding:"0px 8px",
+                 marginTop:"6px", boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px'}}  
+                  onClick={()=>handleLocation(el)}> <GrLocation fontSize='30px' /> {el}
+                <hr /> </Box>
+              ))}
+            </Box>
+
+            <Box >
+            <Box>SUGGESTED PICK UP LOCATIONS</Box>
+            {data2.map((el)=>(
+                <Box style={{height:"70px",boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',padding:"0px 8px",
+                display:"flex", alignItems:"center",marginBottom:"6px",backgroundColor:'white',
+                marginTop:"6px",borderRadius:"8px"}} 
+                onClick={()=>handleLocation(el)} >
+                  <GrLocation fontSize='30px' />{el}</Box>
+              ))}
+            </Box>
+
+          </Box >
+          <Box width="100%" margin='auto'>
+          <Button style={{backgroundColor:"#10a310" ,color:'white',border:"1px solid brown", margin:'auto'   }} >CONFIRM PICKUP LOCATION</Button>
+          </Box>
+           
+        </Box>
     </div>
   )
 }
 
 export default Location
+
+
+const data1 = ["Chandigarh International Airport", "Chandigarh bus stand"]
+
+const data2 = [
+  "Chandigarh International Airport","Chandigarh Railway Station", 'Chandiagrh, India', 'Chandiagrh bus stand',
+  'Chandigarh University', 'Zirakpur Bus Stand'
+]
