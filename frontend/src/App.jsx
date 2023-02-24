cc1970f9a060e77e4d8d4fe387636c8ca5ea6d37
Moumita Das <dasmoumita.me@gmail.com>
2023-02-24 19:41:05 +0530

    login signup complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,11 @@
 
-
-import Payment from "./Pages/Payment/Payment"
-import Profile from "./Pages/Profile/Profile"
-
-import ProductListing from "./Products Pages/ProductListing"
-
-
 import Navbar from "./component/Navbar/Navbar"
 import AllRoutes from "./Routes/AllRoutes"
 function App() {
 
   return (
     <div className="App">
-
-     {/* <Payment/> */}
-     <Profile/>
-
-  <Navbar/>
+    <Navbar/>
     <AllRoutes />
 
     </div>
--- frontend/src/redux/AuthReducer/action.js
+++ frontend/src/redux/AuthReducer/action.js
@@ -1,34 +1,42 @@
-import * as types from "./actionTypes";
 import axios from "axios";
-import { backend_url } from "../../Utils/backendURL";
+import { setToast } from "../Utils/tost";
+import * as data from "./actionTypes"
 
-export const getUserRequest = () => {
-  return {
-    type: types.GET_USER_REQUEST,
-  };
+const signUp = (payload, toast, navigate) => (dispatch) => {
+    dispatch({ type: data.SIGNUP_FAILURE });
+    return axios
+        .post("https://worrisome-tick-tights.cyclic.app/users/register", payload)
+        .then((res) => {
+            console.log(res)
+            // setToast(toast, "Signup Successful", "success");
+            setToast(toast, "Signup Successful", "success");
+
+            dispatch({ type: data.SIGNUP_SUCCESS, payload: res.data });
+            navigate("/login")
+        })
+        .catch((err) => {
+            setToast(toast, err.response.data.message, "error");
+            dispatch({ type: data.SIGNUP_FAILURE, payload: err });
+        });
 };
 
-export const getUserSuccess = (payload) => {
-  return {
-    type: types.GET_USER_SUCCESS,
-    payload,
-  };
-};
+const LoginUser = (payload, toast, navigate) => (dispatch) => {
+    dispatch({ type: data.LOGIN_REQUEST })
+    return axios.post("https://worrisome-tick-tights.cyclic.app/users/login", payload).then((res) => {
+        localStorage.setItem("token", res.data.token)
+        localStorage.setItem("userId", res.data._id)
+        setToast(toast, "Login Successfully", "success");
+        dispatch({ type: data.LOGIN_SUCCESS, payload: res.data })
+        // if (res.data.role === "Admin") {
+        //     navigate("/admin")
+        // } else {
+        //     navigate("/")
+        // }
+            navigate("/")
 
-export const getUserFailure = () => {
-  return {
-    type: types.GET_USER_FAILURE,
-  };
-};
-
-export const Admin_request = () => (dispatch) => {
-  dispatch(getUserRequest());
-  return axios
-    .post(`${backend_url}/users/login`, queryParams)
-    .then((r) => {
-      return dispatch(getUserSuccess(r.data.token));
+    }).catch((err) => {
+        setToast(toast, err.response.data.message, "error");
+        dispatch({ type: data.LOGIN_FAILURE, payload: err });
     })
-    .catch((e) => {
-      dispatch(getUserFailure());
-    });
-};
+}
+export { signUp, LoginUser }
